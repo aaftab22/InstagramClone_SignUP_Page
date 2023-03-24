103b719963f4b72ce1cd8386224c3f06a5f76a41
Paras <dudharejiyaparas@gmail.com>
2023-03-25 02:02:21 +0530

    option_menu Update

---
+++ app/src/main/java/com/example/instagramcloneloginregisterpage/MainActivityOne.java
@@ -0,0 +1,19 @@
+package com.example.instagramcloneloginregisterpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivityOne extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_menu,menu);
+        return true;
+
+    }
+
+
+
+
+}
